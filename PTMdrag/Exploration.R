256063c2c111346b4458c311e5db3063a9cc44c2
Arturas <agrauslys@gmail.com>
2017-01-05 17:51:23 +0000

    stuff

--- PTMdrag/Exploration.R
+++ PTMdrag/Exploration.R
@@ -26,3 +26,96 @@ ggplot(data=dataPep, aes(x=Retention..Delta.))+
 ggplot(data=dataPep, aes(x=Retention..Delta., y=X.L.m.z))+
   geom_density_2d()+
   theme_bw()
+
+ggplot(data=dataPep, aes(y=Retention..Delta., x=pepLen, col=as.factor(X.H.Charge)))+
+  geom_density_2d()+
+  facet_grid(.~X.H.Charge)+
+  theme_bw()
+
+ggplot(data=dataPep, aes(y=Retention..Delta., x=pepLen, z=pepLen))+
+  stat_summary_hex(fun=function (x) log10(length(x)))+
+  scale_fill_gradient(low = 'blue', high = 'red')+
+  facet_grid(.~X.H.Charge)+
+  theme_bw()
+
+
+ggplot(data=dataPep, aes(y=Retention..Delta., x=X.H.Mass, z=pepLen))+
+  stat_summary_hex(fun=length)+
+  scale_fill_gradient(low = 'blue', high = 'red')+
+  facet_grid(.~X.H.Charge)+
+  theme_bw()
+
+ggplot(data=dataPep, aes(y=Retention..Delta., x=X.H.Mass, z=Mass.Difference..Delta.))+
+  stat_summary_hex(fun=mean)+
+  scale_fill_gradient(low = 'blue', high = 'red')+
+  facet_grid(.~X.H.Charge)+
+  theme_bw()
+
+ggplot(data=dataPep, aes(y=X.H.Mass, x=pepLen, z=Retention..Delta.))+
+  stat_summary_hex(fun=mean)+
+  scale_fill_gradient(low = 'blue', high = 'red')+
+  facet_grid(.~X.H.Charge)+
+  theme_bw()
+
+# Calculating physicochemical properties of peptides for learning
+require(Peptides)
+
+calcAAComp = function(seq){
+  aacomp(seq)[,2]*0.01
+}
+
+calcPepProps = function(seq){
+  c(lengthpep(seq=seq), # peptide length
+    calcAAComp(seq=seq), # amino acid composition
+    mw(seq=seq), # molecular weight
+    charge(seq),
+    pI(seq=seq), # isoelectric point
+    aindex(seq=seq), # aliphatic index
+    instaindex(seq=seq), # instability index
+    boman(seq=seq), # boman index
+    hydrophobicity(seq=seq), # hydrophobicity index
+    hmoment(seq=seq) # hydrophobicity moment
+  )
+}
+
+AACompNames = c('Tiny', 'Small', 'Aliphatic', 'Aromatic', 'NonPolar', 'Polar', 'Charged', 'Basic', 'Acidic')
+propNames = c('Length',AACompNames, 'Mol_weight', 'Charge', 'pI', 'AliphaticI', 'InstabilityI', 'BomanI', 'Hydrophobicity', 'Hidrophob_moment')
+
+pepProps = do.call('rbind', lapply(dataPep$Peptide, calcPepProps))
+colnames(pepProps) = propNames
+
+#trying a random forest
+library(randomForest)
+
+set.seed(2002)
+N = nrow(pepProps)
+intrain = sample(1:N, floor(0.8*N))
+
+trX = pepProps[intrain,]
+trY = dataPep$Retention..Delta.[intrain]
+teX = pepProps[-intrain,]
+teY = dataPep$Retention..Delta.[-intrain]
+
+RFmod = randomForest(trX, trY)
+preds = predict(RFmod, newdata = unname(teX))
+
+save(list=c('pepProps','RFmod','preds'), file = '~/Projects/ProteoDNN/ProteoDNN/PTMdrag/RFmodel.RData')
+MSE = mean((preds-teY)^2)
+
+# finding closest peptides in properties (looking at outliers)
+
+# distance
+euclDist = function(x,y) sqrt(sum((x - y)^2))
+
+findClosestN = function(x, Y, n, retIdx = T){
+  dists = apply(Y, 1, function(rw) euclDist(rw,x))
+  closest = order(dists)[1:n]
+  if(retIdx) return(closest)
+  else return(Y[closest,])
+}
+
+
+pRFmod = parallelRandomForest::randomForest(trX, trY)
+preds = predict(RFmod, newdata = unname(teX))
+
+
